411e46a83da96e13b0806978d6f07ffdba72900a
gijsbertbrouwer <gijsbertpieterbrouwer@gmail.com>
2013-11-04 23:17:02 +0100

    ruben

---
+++ LittleCompany/Login.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="LittleCompany.GUI.Login" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Ondor</title>
+    
+    <link href="webresources/style/login.css" rel="stylesheet" />
+    <link href="webresources/style/main.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="loginscreen">
+        <div class="header">
+            <div class="logo">Ondor</div>
+            <div class="login">
+                <asp:TextBox runat="server" id="UI_UserName" />
+                <asp:TextBox runat="server" id="UI_Password" />
+                <asp:Label runat="server" ID="UI_Respons" />
+                <asp:Button runat="server" ID="UI_Login" OnClick="UI_Login_Click" Text="Inloggen" />
+            </div>
+        </div>
+
+         <div class="content">
+            <div class="signupform">
+                <asp:TextBox Cssclass="username_signup" runat="server" Id="UI_SignUp_EmailAdress" placeholder="e-mailadres"/>
+                 <asp:TextBox Cssclass="password_signup" runat="server" Id="UI_SignUp_Password" placeholder="wachtwoord"/>
+                 <asp:TextBox Cssclass="company_signup" runat="server" Id="UI_SignUp_OrganisationName" placeholder="organisatie" />
+                 <asp:Label runat="server" ID="UI_SignUp_Respons" />
+                 <asp:Button Cssclass="submit_signup" Text="Aanmelden"  runat="server" ID="UI_SignUp" OnClick="UI_SignUp_Click"  /> 
+
+
+            </div>
+        </div>
+
+
+ 
+    </div>
+    </form>
+</body>
+</html>
---
+++ LittleCompany/Login.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LittleCompany.GUI
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UI_Login_Click(object sender, EventArgs e)
+        {
+            TryLogin(UI_UserName.Text, UI_Password.Text);
+        }
+
+        private void TryLogin(string username, string password)
+        {
+
+            // Login user
+            var token = new BL.Security().Login(username, password);
+
+            if (token != null)
+            {
+                Session["token"] = token;
+                Response.Redirect("webforms/dashboard.aspx");
+            }
+            else
+            {
+                UI_Respons.Text = "Ongeldige credentials, probeer het opnieuw.";
+                Session["token"] = null;
+            }
+
+        }
+
+
+        protected void UI_SignUp_Click(object sender, EventArgs e)
+        {
+
+
+            int mainloginid = new BL.Customer().RegisterNewCustomer(
+                                                        UI_SignUp_OrganisationName.Text,
+                                                        UI_SignUp_EmailAdress.Text,
+                                                        UI_SignUp_Password.Text);
+         
+        if(mainloginid > 0){
+            TryLogin(UI_SignUp_EmailAdress.Text,UI_SignUp_Password.Text);
+        }else{
+            UI_SignUp_Respons.Text="Helaas kunt u deze gegevens niet gebruiken.";
+
+        }
+
+
+
+
+        }
+    }
+}
