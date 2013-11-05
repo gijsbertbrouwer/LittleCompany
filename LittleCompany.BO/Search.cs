5c5a32aee0de6c12e1864452211707febe04dfd0
gijsbertbrouwer <gijsbertpieterbrouwer@gmail.com>
2013-11-05 21:22:22 +0100

    Quick search v0.1

---
+++ LittleCompany.BO/Search.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LittleCompany.BO
+{
+    public class Search
+    {
+
+        public class Quick
+        {
+            public string query { get; set; }
+            public int customerid { get; set; }
+            public int searchDataTypeId { get; set; }
+
+
+            public List<item> searchresults { get; set; }
+
+            public class item
+            {
+                public int id { get; set; }
+                public string name { get; set; }
+                public int datatypeid { get; set; }
+
+            }
+
+        }
+
+    }
+}
---
+++ LittleCompany.DAL/db scripts/20131105-Add organisation customerid.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Organisation] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (250) NULL,
+    [CustomerId] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_organisation_To_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([Id]),
+);
+
+
+CREATE TABLE [dbo].[DataTypes] (
+    [Id]               INT NOT NULL,
+    [TableName]        NVARCHAR (250) NOT NULL,
+    [TableNameCaption] NVARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
