b49de9097fda8597bed6c148cf3d60af846389d9
gijsbertbrouwer <gijsbertpieterbrouwer@gmail.com>
2013-11-05 21:55:21 +0100

    quicksearch v0.2

---
+++ LittleCompany.DAL/db scripts/20131105-QuickSearch.sql
@@ -0,0 +1,101 @@
+﻿
+
+CREATE TABLE [dbo].[Organisation] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (250) NULL,
+    [CustomerId] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_organisation_To_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([Id]),
+);
+
+
+CREATE TABLE [dbo].[DataTypes] (
+    [Id]               INT NOT NULL,
+    [TableName]        NVARCHAR (250) NOT NULL,
+    [TableNameCaption] NVARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+
+CREATE PROCEDURE [dbo].Search_QuickAccess_Person
+	@query nvarchar(250),
+	@customerid int
+
+AS
+	SELECT top(10) Id, Name, 3 as datatypeId from Person 
+	where  
+	CustomerId = @customerid and
+	Name like '%' +  @query + '%'
+
+
+
+	CREATE PROCEDURE [dbo].Search_QuickAccess_File
+	@query nvarchar(250),
+	@customerid int
+
+AS
+	SELECT top(10) Id, Name, 1 as datatypeId from Files
+	where  
+	CustomerId = @customerid and
+	Name like '%' +  @query + '%'
+
+
+
+
+
+
+CREATE PROCEDURE [dbo].Search_QuickAccess_Organisation
+	@query nvarchar(250),
+	@customerid int
+
+AS
+	SELECT top(10) Id, Name, 2 as datatypeId from Organisation 
+	where  
+	CustomerId = @customerid and
+	Name like '%' +  @query + '%'
+
+CREATE PROCEDURE [dbo].Search_QuickAccess_Main
+	@query nvarchar(250),
+	@customerid int,
+	@datatypeid int
+AS
+	
+-- Create a temp table for storing the results
+CREATE TABLE #sp_SearchQuick_Items
+( 
+    Id int, 
+    name nvarchar(250) NULL,
+	datatypeId int
+) 
+
+
+
+ if @datatypeid is null or @datatypeid < 1 or @datatypeid = 2
+ BEGIN
+	INSERT #sp_SearchQuick_Items EXEC Search_QuickAccess_File @query, @customerid
+ END 
+
+
+ if @datatypeid is null or @datatypeid < 1 or @datatypeid = 2
+ BEGIN
+	INSERT #sp_SearchQuick_Items EXEC Search_QuickAccess_Organisation @query, @customerid
+ END 
+
+ if @datatypeid is null or @datatypeid < 1 or @datatypeid = 3
+ BEGIN
+	INSERT #sp_SearchQuick_Items EXEC Search_QuickAccess_Person @query, @customerid
+ END 
+
+
+
+
+
+
+SELECT top(10) Id, Name, datatypeId FROM #sp_SearchQuick_Items  order by name
+
+ 
+
+ -- Drop temporary tables
+DROP TABLE #sp_SearchQuick_Items
