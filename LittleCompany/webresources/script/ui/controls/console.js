08e9e32361a01ada304b2925c792dbfc2b044a5d
rubenduiveman <ruben.duiveman@gmail.com>
2013-11-08 14:46:36 +0100

    console edit

--- LittleCompany/webresources/script/ui/controls/console.js
+++ LittleCompany/webresources/script/ui/controls/console.js
@@ -2,10 +2,13 @@
     var me = this;
 
     var skin;
-    var log = [];
+    var logsDOM;
+    var logs = [];
 
     me.openstring = "open console";
     var openstringmatch = "";
+
+    var commandinput;
 
     var stringutil;
 
@@ -13,20 +16,31 @@
         ox.Log = me.Log;
 
         skin = $('#console');
+        logsDOM = skin.find('.logs');
+        commandinput = skin.find('.commandlineinput');
 
         stringutil = new Stringutil();
 
         AddClosedEventListeners();
+
+        for (var i = 0; i < 100; i++) {
+            me.Log("TEST LOG" + i);
+        }
     };
 
     me.Log = function (message) {
-        log.push(message);
+        logs.push(message);
     };
 
 
     me.Open = function () {
         skin.show();
+
+        RenderLogs();
+
         AddOpenEventListeners();
+
+        commandinput.focus();
     };
 
     me.Close = function () {
@@ -34,6 +48,28 @@
         AddClosedEventListeners();
     };
 
+    
+
+    var RenderLogs = function () {
+        var i = 0, l = logs.length, log, logskin = "";
+
+        for (i; i < l; i++) {
+            log = logs[i];
+
+            logskin += "<div class='log'>" + log + "</div>";
+            
+        }
+
+        
+        logsDOM.append(logskin);
+        logsDOM.animate({ scrollTop: logsDOM[0].scrollHeight }, 0);
+    };
+
+    var HandleCommand = function (c) {
+
+        logsDOM.append("<div class='log command'>" + c + "</div>");
+        logsDOM.animate({ scrollTop: logsDOM[0].scrollHeight }, 100);
+    };
 
 
     var AddClosedEventListeners = function () {
@@ -46,18 +82,27 @@
         if (me.openstring == openstringmatch) {
             openstringmatch = "";
             me.Open();
-            return;
+            return false;
         }
 
         if (!stringutil.StartsWith(me.openstring, openstringmatch)) {
             openstringmatch = "";
         }
+
     };
 
 
 
     var AddOpenEventListeners = function () {
+        skin.find('#commandform').unbind('submit').submit(OnCommandSubmit);
+    };
 
+    var OnCommandSubmit = function () {
+        var c = commandinput.val();
+        HandleCommand(c);
+
+        commandinput.val("");
+        return false;
     };
 
 }
