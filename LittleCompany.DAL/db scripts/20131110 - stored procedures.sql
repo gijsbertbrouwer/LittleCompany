967ce5ba67daf9472e89c5e377a5da4a66cf7a77
gijsbertbrouwer <gijsbertpieterbrouwer@gmail.com>
2013-11-10 21:55:00 +0100

    sp

---
+++ LittleCompany.DAL/db scripts/20131110 - stored procedures.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].File_Get
+	@fileid int,
+	@customerid int
+AS
+	SELECT id, name, organisationid, personid, customerid, [path], dateupload, [version], [guid], [password]
+	From Files
+	where CustomerId = @customerid and id = @fileid
+RETURN 0
